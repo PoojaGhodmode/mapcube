7cd3406636118a19aadce423336c3b3b6b39274d
poojaghodmode <poojaghodmode@gmail.com>
2021-08-26 16:10:43 +0530

    Add normal map

---
+++ src/components/MapContainer/index.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "./MapContainer.css";
+const MAPBOX_TOKEN = process.env.REACT_APP_MAP_TOKEN;
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+const MapContainer = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(82);
+  const [lat, setLat] = useState(21);
+  const [zoom, setZoom] = useState(3);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+};
+
+export default MapContainer;
