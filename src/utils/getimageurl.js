bcd40f8c28ff6640f1b4445c5381ca9cd141ea42
poojaghodmode <poojaghodmode@gmail.com>
2021-08-29 19:02:40 +0530

    change image size

--- src/utils/getimageurl.js
+++ src/utils/getimageurl.js
@@ -1,7 +1,7 @@
 const getImageURL = (lng, lat, zoom) => {
-  const url1 = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${lng},${lat},${zoom},0/500x500?access_token=${process.env.REACT_APP_MAP_TOKEN}`;
-  
-  const url2 = `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/${lng},${lat},${zoom},0/500x500?access_token=${process.env.REACT_APP_MAP_TOKEN}`;
+  const url1 = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${lng},${lat},${zoom},0/600x600?access_token=${process.env.REACT_APP_MAP_TOKEN}`;
+
+  const url2 = `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/${lng},${lat},${zoom},0/600x600?access_token=${process.env.REACT_APP_MAP_TOKEN}`;
   return {
     url1,
     url2,
